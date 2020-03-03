945178c751e016e543607eb1dc6b59f04dd7e282
Sheldon Choo <sheldon.choo@ontariotechu.net>
2020-03-03 18:40:06 -0500

    Added angle labels

--- src/questions/Question_3.java
+++ src/questions/Question_3.java
@@ -5,6 +5,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -15,7 +16,6 @@ public class Question_3 extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         //3 points of the triangle
         Circle[] points = new Circle[3];
         for(int i = 0; i < 3; i++) {
@@ -34,6 +34,9 @@ public class Question_3 extends Application {
         Line a = new Line(points[1].getCenterX(),points[1].getCenterY(),points[2].getCenterX(),points[2].getCenterY());
         Line b = new Line(points[2].getCenterX(),points[2].getCenterY(),points[0].getCenterX(),points[0].getCenterY());
         Line c = new Line(points[0].getCenterX(),points[0].getCenterY(),points[1].getCenterX(),points[1].getCenterY());
+        double aLen= lineLength(a);
+        double bLen = lineLength(b);
+        double cLen = lineLength(c);
 
         //Bind lines to points
         a.startXProperty().bind(points[1].centerXProperty());
@@ -51,6 +54,19 @@ public class Question_3 extends Application {
         c.endXProperty().bind(points[1].centerXProperty());
         c.endYProperty().bind(points[1].centerYProperty());
 
+        //Labels for the angles (set initial values)
+        Label A = new Label(String.valueOf(toDegrees((Math.acos((aLen*aLen-bLen*bLen-cLen*cLen)/(-2*bLen*cLen))))));
+        A.translateXProperty().bind(points[0].centerXProperty());
+        A.translateYProperty().bind(points[0].centerYProperty());
+
+        Label B = new Label(String.valueOf(toDegrees((Math.acos((bLen*bLen-aLen*aLen-cLen*cLen)/(-2*aLen*cLen))))));
+        B.translateXProperty().bind(points[1].centerXProperty());
+        B.translateYProperty().bind(points[1].centerYProperty());
+
+        Label C = new Label(String.valueOf(toDegrees((Math.acos((cLen*cLen-bLen*bLen-aLen*aLen)/(-2*aLen*bLen))))));
+        C.translateXProperty().bind(points[2].centerXProperty());
+        C.translateYProperty().bind(points[2].centerYProperty());
+
         //The circle
         Circle perimeter = new Circle();
         perimeter.setCenterX(200);
@@ -63,34 +79,51 @@ public class Question_3 extends Application {
         for(int i = 0; i < 3; i++) {
             Circle temp = points[i];
             temp.setOnMouseDragged(e->{
-                //Determine point on the perimeter closest to the mouse position
                 //Determine angle
                 double theta = (Math.atan((e.getY()-200)/(e.getX()-200)));
 
-                //Position the point
+                //Position the point on the circle
                 if(e.getX() < 200) {
                     temp.setCenterX(-(100*Math.cos(theta))+200);
                     temp.setCenterY(-(100*Math.sin(theta))+200);
                 }
                 else {
-                    temp.setCenterX(100 * Math.cos(theta) + 200);
-                    temp.setCenterY(100 * Math.sin(theta) + 200);
+                    temp.setCenterX(100*Math.cos(theta)+200);
+                    temp.setCenterY(100*Math.sin(theta)+200);
                 }
+
+                //Calculate the angles of the triangle
+                double lenA = lineLength(a);
+                double lenB = lineLength(b);
+                double lenC = lineLength(c);
+
+                A.setText(String.valueOf(toDegrees((Math.acos((lenA*lenA-lenB*lenB-lenC*lenC)/(-2*lenB*lenC))))));
+                B.setText(String.valueOf(toDegrees((Math.acos((lenB*lenB-lenA*lenA-lenC*lenC)/(-2*lenA*lenC))))));
+                C.setText(String.valueOf(toDegrees((Math.acos((lenC*lenC-lenB*lenB-lenA*lenA)/(-2*lenA*lenB))))));
             });
         }
 
         //Add all the shapes to the group
         Group root = new Group();
         root.getChildren().add(perimeter);
-        root.getChildren().add(a);
-        root.getChildren().add(b);
-        root.getChildren().add(c);
+        root.getChildren().addAll(a,b,c);
         root.getChildren().addAll(points);
+        root.getChildren().addAll(A,B,C);
 
         Scene scene = new Scene(root,400,400);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Question_3");
         primaryStage.show();
+    }
+
+    //Converts radians into degrees
+    public int toDegrees(double radians) {
+        return (int)(radians*(180/Math.PI));
+    }
+
+    //Calculates the length of a line segment
+    public double lineLength(Line line) {
+        return Math.sqrt(Math.pow(line.getEndX()-line.getStartX(),2)+Math.pow(line.getEndY()-line.getStartY(),2));
     }
 
     public static void main(String[] args) {
